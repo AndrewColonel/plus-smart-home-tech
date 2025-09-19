65378475c3fabe6f6474e2c751e90acae0d6154f
AndrewColonel <andrewcolonel@yandex.ru>
2025-09-19 23:10:10 +0300

    avro schemas

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
@@ -1,4 +1,32 @@
 package ru.yandex.practicum.telemetry.collector.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.telemetry.collector.model.HubEvent;
+import ru.yandex.practicum.telemetry.collector.model.SensorEvent;
+
+import javax.validation.Valid;
+
+@RestController
+@AllArgsConstructor
+@Validated
+@RequestMapping("/events")
+@Slf4j
 public class EventController {
+
+    @PostMapping("/sensors")
+    public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+        log.info(event.toString());
+    }
+
+    @PostMapping("/hubs")
+    public void collectHubEvent(@Valid @RequestBody HubEvent event) {
+        log.info(event.toString());
+    }
+
 }
