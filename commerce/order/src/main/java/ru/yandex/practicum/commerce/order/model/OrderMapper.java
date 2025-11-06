c2cd1653cf9351a8499c956e97ac42e305ac7d68
AndrewColonel <andrewcolonel@yandex.ru>
2025-11-06 14:48:12 +0300

    sht - commerce - order - #2

---
+++ commerce/order/src/main/java/ru/yandex/practicum/commerce/order/model/OrderMapper.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.commerce.order.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import ru.yandex.practicum.commerce.iteraction.api.dto.order.OrderDto;
+import ru.yandex.practicum.commerce.order.model.entity.Order;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderMapper {
+
+public static OrderDto toDto(Order order) {
+    return OrderDto.builder()
+            .orderId(order.getOrderId())
+            .shoppingCartId(order.getShoppingCartId())
+            .products(order.getProducts())
+            .paymentId(order.getPaymentId())
+            .deliveryId(order.getDeliveryId())
+            .state(order.getState())
+            .deliveryWeight(order.getDeliveryWeight())
+            .deliveryVolume(order.getDeliveryVolume())
+            .fragile(order.getFragile())
+            .totalPrice(order.getTotalPrice())
+            .deliveryPrice(order.getDeliveryPrice())
+            .productPrice(order.getProductPrice())
+            .build();
+}
+
+
+public static Order toEntity(OrderDto orderDto) {
+    return Order.builder()
+            .orderId(orderDto.getOrderId())
+            .shoppingCartId(orderDto.getShoppingCartId())
+            .products(orderDto.getProducts())
+            .paymentId(orderDto.getPaymentId())
+            .deliveryId(orderDto.getDeliveryId())
+            .state(orderDto.getState())
+            .deliveryWeight(orderDto.getDeliveryWeight())
+            .deliveryVolume(orderDto.getDeliveryVolume())
+            .fragile(orderDto.getFragile())
+            .totalPrice(orderDto.getTotalPrice())
+            .deliveryPrice(orderDto.getDeliveryPrice())
+            .productPrice(orderDto.getProductPrice())
+            .build();
+}
+
+}
---
+++ commerce/order/src/main/resources/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS order_products;
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE IF NOT EXISTS orders(
+    order_id UUID PRIMARY KEY,
+    shopping_cart_id UUID NOT NULL,
+    payment_id UUID,
+    delivery_id UUID,
+    state VARCHAR,
+    delivery_weight DECIMAL(6, 2) CHECK (delivery_weight > 0),
+    delivery_volume DECIMAL(6, 2) CHECK (delivery_volume > 0),
+    fragile BOOLEAN,
+    total_price DECIMAL(6, 2) CHECK (total_price > 0),
+    delivery_price DECIMAL(6, 2) CHECK (delivery_price > 0),
+    product_price DECIMAL(6, 2) CHECK (product_price > 0),
+    UNIQUE(shopping_cart_id, payment_id, delivery_id)
+);
+
+CREATE TABLE IF NOT EXISTS order_products(
+    order_id UUID NOT NULL REFERENCES orders(order_id) ON DELETE CASCADE,
+    product_id UUID NOT NULL,
+    quantity BIGINT NOT NULL CHECK (quantity >= 0),
+    PRIMARY KEY (order_id, product_id)
+);
